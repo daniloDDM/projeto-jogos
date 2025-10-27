d9de25239fd5a4065943d19510721670ffc9305d
Iago Rosa <iagoimportantthings@gmail.com>
2025-10-27 15:57:32 -0300

    feat: iniciando player

---
+++ Assets/Player.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public KeyCode moveRight = KeyCode.RightArrow;
+    public KeyCode moveLeft = KeyCode.LeftArrow;
+    public KeyCode jump = KeyCode.UpArrow;
+    //public KeyCode fire = KeyCode.Space;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float boundX = 13.0f;
+    public float boundY = 4.5f;
+
+    //public GameObject tiroPrefab;
+    //public Transform firePoint;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("pisando", isGrounded);
+        animator.SetBool("andandoDireita", false);
+        animator.SetBool("andandoEsquerda", false);
+
+        float horizontalDirection = 0.0f;
+
+        if (Input.GetKey(moveRight))
+        {
+            animator.SetBool("andandoDireita", true);
+            horizontalDirection = 1.0f;
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            animator.SetBool("andandoDireita", true);
+            //animator.SetBool("isMoving", true);
+            horizontalDirection = -1.0f;
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        var hVelocity = rb.velocity;
+        hVelocity.x = horizontalDirection * moveSpeed;
+        rb.velocity = hVelocity;
+
+        var hPosition = transform.position;
+        hPosition.x = Mathf.Clamp(hPosition.x, -boundX, boundX);
+        transform.position = hPosition;
+
+        //if (Input.GetKeyDown(fire))
+        //{
+        //    firePoint = transform.Find("FirePoint");
+        //    Instantiate(tiroPrefab, firePoint.position, Quaternion.identity);
+        //}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plataforma"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plataforma"))
+        {
+            isGrounded = false;
+        }
+    }
+}
